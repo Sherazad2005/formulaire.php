1d292fc7d963ba3feea54159f33b896113795bc3
Sherazad Abdallah <s.abdallah@lprs.fr>
2023-12-17 17:28:30 -0800

    insert

--- formulaire.php/connecter.php
+++ formulaire.php/connecter.php
@@ -2,13 +2,31 @@
 
 $user = "root";
 $pass = "";
+$user = $_POST['id_user'];
+$metier=$_POST['metier'];
 
 
 $php_formulaire = new PDO('mysql:host=localhost;dbname=php_formulaire;charset=utf8', $user, $pass);
 $php_formulaire->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$sql = "SELECT * FROM members";
-$stmt = $php_formulaire->prepare($sql);
-$stmt->execute();
+$membres = $php_formulaire->query("SELECT  id_user,nom, prenom FROM user");
+while ($row = $membres->fetch_assoc()) {
+    echo "<option value='" . $row['id_user'] . "'>" . $row['nom'] . "'>" . $row['prenom'] . "</option>";
+}
 
-$members = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$req = $php_formulaire->prepare('INSERT INTO user(metier) VALUES(troisD) WHERE id_user = ');
+$req->execute(array(
+
+
+    ':troisD' => $metier,
+
+
+
+));
+    $php_formulaire->close();
+
+?>
+
+
+
+
